3a6736a0695a4596bafc8b31ffefc7bb1f3f530c
Reo Hakuta <st20152ro@gm.ibaraki-ct.ac.jp>
2024-01-05 22:42:39 +0900

    feat: ✨ (`web` `<PhotoCard>`) チェキ風の見た目の画像コンポーネントを追加した。

---
+++ apps/web/src/components/PhotoCard/PhotoCard.tsx
@@ -0,0 +1,67 @@
+import { type ReactNode } from 'react';
+import { Image, type ImageProps } from '@/components/Image/Image';
+import { cx, css, sva } from 'styled-system/css';
+import { flex } from 'styled-system/patterns';
+import type { SystemStyleObject, RecipeVariantProps } from 'styled-system/types';
+
+export const photoCardSlotRecipe = sva({
+  slots: ['root', 'image', 'label'],
+  base: {
+    root: {
+      w: '48',
+      h: 'fit-content',
+      flexShrink: '0',
+      bg: 'slate.2',
+      shadow: 'lg',
+      p: '4',
+      display: 'flex',
+      flexDirection: 'column',
+      justifyContent: 'center',
+      alignItems: 'center',
+      gap: '2',
+    },
+    image: {
+      w: 'full',
+      objectFit: 'cover',
+      bg: 'slate.3',
+    },
+    label: {
+      fontFamily: 'code',
+      lineHeight: '1.25',
+      color: 'slate.11',
+    },
+  },
+});
+
+export type PhotoCardProps = {
+  css?: SystemStyleObject[];
+  chilsren?: ReactNode;
+  timestamp?: string;
+} & RecipeVariantProps<typeof photoCardSlotRecipe> &
+  ImageProps;
+
+/**
+ * Renders a component.
+ * @param css Style props in PandaCSS format. e.g. `[{ w: 'full', bg: 'cyan.9' }]`
+ * @param children Children `ReactNode` to render.
+ */
+export const PhotoCard = ({
+  css: cssProps = [{}],
+  alt,
+  children,
+  timestamp,
+  className,
+  ...props
+}: PhotoCardProps): ReactNode => {
+  const { root, image, label } = photoCardSlotRecipe.raw(props);
+  return (
+    <figure className={cx(css(root, ...cssProps), className)}>
+      {/* eslint-disable-next-line jsx-a11y/alt-text */}
+      <Image css={[image]} alt={alt} {...props} />
+      <div className={flex({ w: 'full', direction: 'row', justify: 'center', align: 'center', gap: '1' })}>
+        <figcaption className={css(label, { display: 'block', flexGrow: '1' })}>{children}</figcaption>
+        <figcaption className={css(label)}>{timestamp}</figcaption>
+      </div>
+    </figure>
+  );
+};
