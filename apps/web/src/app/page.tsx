b8d73c8a26d48049f82f7c84d74f279e86430e58
Reo Hakuta <st20152ro@gm.ibaraki-ct.ac.jp>
2024-01-05 23:47:28 +0900

    feat: ✨ (`web`) JSON-LDを追加した。
    - `@type`: `person`のみ追加した。

--- apps/web/src/app/page.tsx
+++ apps/web/src/app/page.tsx
@@ -1,10 +1,12 @@
 import type { ReactNode } from 'react';
 import { Hero } from '@/components/Hero/Hero';
+import { JsonLdScript } from '@/components/JsonLdScript/JsonLdScript';
 import { MyPhotoCardStack } from '@/components/MyPhotoCardStack/MyPhotoCardStack';
 
 const Home = (): ReactNode => {
   return (
     <>
+      <JsonLdScript type="person" />
       <Hero />
       <MyPhotoCardStack />
     </>
