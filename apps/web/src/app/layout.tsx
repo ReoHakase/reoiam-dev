619ec36dfd5fdc4914193287a2bc49be6195e693
Reo Hakuta <st20152ro@gm.ibaraki-ct.ac.jp>
2024-01-05 23:46:38 +0900

    refactor: ♻️ (`web`) 現在のベースURLを別ファイルに切り分けた。

--- apps/web/src/app/layout.tsx
+++ apps/web/src/app/layout.tsx
@@ -4,6 +4,7 @@ import type { FC, ReactNode } from 'react';
 import { Header } from '@/components/Header/Header';
 import { AppProvider } from '@/providers';
 import { fontVariables } from '@/styles/fonts';
+import { baseUrl } from '@/utils/routes/baseUrl';
 import { css } from 'styled-system/css';
 import '@/styles/globals.css';
 
@@ -50,15 +51,8 @@ const RootLayout: FC<RootLayoutProps> = ({ children }) => (
 
 export default RootLayout;
 
-// When VERCEL_URL is detected: https://${process.env.VERCEL_URL}
-// If there's no environment variable VERCEL_URL is set, will always fallback to http://localhost:${process.env.PORT || 3000}.
-// Refer: https://beta.nextjs.org/docs/api-reference/metadata#metadatabase
-const domain = new URL(
-  process.env.VERCEL_URL ? `https://${process.env.VERCEL_URL}` : `http://localhost:${process.env.PORT || 3000}`,
-);
-
 export const metadata: Metadata = {
-  metadataBase: domain,
+  metadataBase: baseUrl,
 
   title: {
     default: 'Reo Hakuta | reoiam.dev',
@@ -71,7 +65,7 @@ export const metadata: Metadata = {
     // Refer: https://beta.nextjs.org/docs/api-reference/metadata#static-images
     type: 'website',
     locale: 'en_US',
-    url: domain,
+    url: baseUrl,
   },
 };
 
