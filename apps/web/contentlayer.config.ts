b82054be95afae79be3cdeb8c4a2e6cbb439ed0e
Reo Hakuta <st20152ro@gm.ibaraki-ct.ac.jp>
2024-01-27 00:36:21 +0900

    build: 📦️ (`contentlayer`) MDX用のremark, rehypeのプラグインを追加した。

--- apps/web/contentlayer.config.ts
+++ apps/web/contentlayer.config.ts
@@ -1,8 +1,13 @@
 import { defineDocumentType, makeSource } from 'contentlayer/source-files';
+import { rehypeGithubAlerts } from 'rehype-github-alerts';
+import rehypeKatex from 'rehype-katex';
+import rehypePrettyCode from 'rehype-pretty-code';
+import remarkGfm from 'remark-gfm';
+import remarkMath from 'remark-math';
 
 export const ContentDocument = defineDocumentType(() => ({
   name: 'ContentDocument',
-  filePathPattern: `**/*.mdx`,
+  filePathPattern: '**/*.mdx',
   contentType: 'mdx',
   fields: {
     title: { type: 'string', required: true },
@@ -13,6 +18,28 @@ export const ContentDocument = defineDocumentType(() => ({
   },
 }));
 
-const source: ReturnType<typeof makeSource> = makeSource({ contentDirPath: 'docs', documentTypes: [ContentDocument] });
+const source: ReturnType<typeof makeSource> = makeSource({
+  contentDirPath: 'docs',
+  documentTypes: [ContentDocument],
+  mdx: {
+    remarkPlugins: [remarkGfm, remarkMath],
+    rehypePlugins: [
+      [
+        // @ts-expect-error TODO: Fix the type error, which seems to be caused by incorrect type definition provided by contentlayer
+        rehypePrettyCode,
+        {
+          theme: {
+            light: 'github-light',
+            dark: 'github-dark',
+          },
+          keepBackground: false,
+        },
+      ],
+      rehypeGithubAlerts,
+      // @ts-expect-error TODO: Fix the type error, which seems to be caused by incorrect type definition provided by contentlayer
+      rehypeKatex,
+    ],
+  },
+});
 
 export default source;
